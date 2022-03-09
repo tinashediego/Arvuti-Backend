2a4e185da34de6176bef891adba642c79ac54bd0
tinlee <tinashediego@gmail.com>
2022-03-09 12:33:43 +0200

    cors

--- src/main/java/com/tinlee/app/rest/Controller/UserController.java
+++ src/main/java/com/tinlee/app/rest/Controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
     @Autowired
